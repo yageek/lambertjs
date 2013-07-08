24b93ae5b90a4195ba60fbf51e717652fa9e513b
HEINRICH Yannick <yannick.heinrich@gmail.com>
2013-07-08 12:42:05 +0200

    Add package.json and add convert function to addon

--- lambert.cc
+++ lambert.cc
@@ -5,14 +5,70 @@
 
 using namespace v8;
 
-Handle<Value> Method(const Arguments& args) {
-  HandleScope scope;
-  return scope.Close(String::New("world"));
+
+Handle<Value> lambertTowgs84(const Arguments& args){
+	HandleScope scope;
+
+	if(args.Length() <2)
+	{
+		if (args.Length() < 2) {
+		    ThrowException(Exception::TypeError(String::New("Wrong number of arguments!")));
+		    return scope.Close(Undefined());
+	  }
+	}
+
+	if(!args[0]->IsObject() || !args[1]->IsNumber())
+	{
+		 ThrowException(Exception::TypeError(String::New("Wrong arguments!")));
+    	 return scope.Close(Undefined());
+	}
+
+	Local<Object> jsObj = args[0]->ToObject();	
+	if(!jsObj->HasRealNamedProperty(String::New("x")) || !jsObj->HasRealNamedProperty(String::New("y")) || !jsObj->HasRealNamedProperty(String::New("z")))
+	{
+		 ThrowException(Exception::TypeError(String::New("Object has no property x,y and z!")));
+    	 return scope.Close(Undefined());
+	}
+
+	double lat, lon, h;
+
+	if(!jsObj->Get(String::New("x"))->IsNumber() || !jsObj->Get(String::New("y"))->IsNumber() || !jsObj->Get(String::New("z"))->IsNumber() )
+	{
+		
+		ThrowException(Exception::TypeError(String::New("x,y and z properties are not all numbers!")));
+    	return scope.Close(Undefined());
+	}
+
+	lon = jsObj->Get(String::New("x"))->NumberValue();
+	lat = jsObj->Get(String::New("y"))->NumberValue();
+	h = jsObj->Get(String::New("z"))->NumberValue();
+
+
+	long long val = args[1]->ToInteger()->Value();
+	
+	if(val > 5 || val < 0)
+	{
+		ThrowException(Exception::TypeError(String::New("Incorrect zone was given!")));
+    	 return scope.Close(Undefined());
+	}
+	
+	LambertZone zone = (LambertZone) val;
+	Point org = {lon,lat,h},dest = {0,0,0};
+
+	lambert_to_wgs84(&org,&dest,zone);
+
+	Local<Object> point = Object::New();
+	point->Set(String::NewSymbol("x"),Number::New(lon));
+	point->Set(String::NewSymbol("y"),Number::New(lat));
+	point->Set(String::NewSymbol("z"),Number::New(h));
+
+	return scope.Close(point);
+
 }
 
 void init(Handle<Object> exports) {
-  exports->Set(String::NewSymbol("hello"),
-      FunctionTemplate::New(Method)->GetFunction());
+  exports->Set(String::NewSymbol("lambertTowgs84"),
+      FunctionTemplate::New(lambertTowgs84)->GetFunction());
 }
 
 NODE_MODULE(lambert, init)
